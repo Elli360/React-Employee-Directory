bd882c8a5f95bc441a2ca3a161ede7d20c3fa5ba
Danielli <ellid85@gmail.com>
2020-12-21 16:20:10 -0500

    edits to API call

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -12,3 +12,35 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+
+
+// Question 1 for TA
+// Where to add the API call? do i need a components folder?
+// Source Activity 19
+// import React from "react";
+// import SearchResultContainer from "./components/SearchResultContainer";
+
+// function App() {
+//   return <SearchResultContainer />;
+// }
+
+// export default App;
+//Question 2
+// Source Activity 19
+// Do I need a Utils folder> file API.js for the axious
+// import axios from "axios";
+
+// const BASEURL = "https://api.giphy.com/v1/gifs/search?q=";
+// const APIKEY = "&api_key=dc6zaTOxFJmzC&limit=20";
+
+// // Export an object with a "search" method that searches the Giphy API for the passed query
+// export default {
+//   search: function(query) {
+//     return axios.get(BASEURL + query + APIKEY);
+//   }
+// };
+
