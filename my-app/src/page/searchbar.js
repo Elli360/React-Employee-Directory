f91fac10634a4b3a4883f2ab9e2c74720b0413cb
Danielli <ellid85@gmail.com>
2020-12-21 20:40:43 -0500

    search-form code edits

--- my-app/src/page/searchbar.js
+++ my-app/src/page/searchbar.js
@@ -1,22 +1,22 @@
 // sample code 
 import React from "react";
 
-function SearchForm() {
+function SearchForm(props) {
   return (
         <form>
-          <div className="form-group">
+          <div className="form-group"> style ={{ display: 'flex' }}
             <label htmlFor="search">Search:</label>
             <input
-            //   onChange={handleInputChange} 
-              value={search}
+              onChange={props.handleInputChange} 
+              value={props.value}
               name="search"
               type="text"
               className="form-control"
-              placeholder="Search For a Movie"
+              placeholder="Search Employee"
               id="search"
             />
             <br />
-            <button onClick={handleFormSubmit} className="btn btn-primary">
+            <button onClick={props.handleFormSubmit} className="btn btn-primary">
               Search
             </button>
           </div>
